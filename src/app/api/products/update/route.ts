db132937ff4fb85aece9559b4a3dddc5d17adaff
Meraf <97203234+meraf@users.noreply.github.com>
2026-06-20 17:40:59 +0300

    Fix import statement for prisma client

--- src/app/api/products/update/route.ts
+++ src/app/api/products/update/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '../../../lib/prisma'; // Adjust this path if your prisma client is exported from somewhere else
-
+import prisma from '../../../lib/prisma';
 export async function GET() {
   try {
     const products = await prisma.product.findMany({
